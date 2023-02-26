c5332af7f04b6759b22d48bab876ba80b6755137
WidthDoctor <elskel1812@gmail.com>
2023-02-26 12:53:54 +0100

    feat: completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.smallNum = min;
+      this.bigNum = max;
     }
 
     guess() {
-
+      return  Math.round((this.smallNum + this.bigNum)/2);
     }
 
     lower() {
-
+      this.bigNum = this.guess()
     }
 
     greater() {
-
+      this.smallNum = this.guess()
     }
 }
 
